6c3cb0990660236bb0b3f18d479f38e9dc7ea81e
lilianabulgaru3pg <lilianabulgaru@3pillarglobal.com>
2018-03-31 19:04:13 +0300

    origin/work/edit-category - added edit row component

---
+++ SweetsFactory/src/app/shared/table-row/row.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'table-row',
+  templateUrl: './row.component.html',
+  styleUrls: ['./row.component.css'],
+})
+
+export class RowComponent {
+  constructor() { }
+}
